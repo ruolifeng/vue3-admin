c4acfe36c7a60e17281c04f954e79e58e548cc32
sunjianrong <sunruolifeng@gmail.com>
2024-02-21 17:57:42 +0800

    feat: 配置代理解决跨域访问问题

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,16 @@ export default defineConfig({
     server: {
         port: 8888, // 端口号
         open: true, // 启动项目自动打开浏览器访问
-        host: 'localhost' // 若指定0.0.0.0则开放所有ip访问
+        host: 'localhost', // 若指定0.0.0.0则开放所有ip访问
+        // 配置代理解决跨域问题
+        proxy: {
+            '/dev-api': {
+                target: 'https://mock.mengxuegu.com/mock/65d57e8d351bbd02cf339ca9/vue3-admin',
+                rewrite(path) {
+                    return path.replace(/^\/dev-api/, '');
+                },
+            }
+        }
     },
     plugins: [
         vue(),
