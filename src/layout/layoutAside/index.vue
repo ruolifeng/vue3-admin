e041d4b118c37c90d52a56a9e8f6c65a75a6a3f0
sunjianrong <sunruolifeng@gmail.com>
2024-02-21 22:36:00 +0800

    feat: logo实现

--- src/layout/layoutAside/index.vue
+++ src/layout/layoutAside/index.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import {defineAsyncComponent} from "vue";
 
+const Logo = defineAsyncComponent(() => import('@/layout/layoutAside/logo.vue'))
 </script>
 
 <template>
-<!--  左边菜单栏-->
+  <!--  左边菜单栏-->
   <div class="h100">
-    <el-aside class="layout-aside layout-aside-menu-200">Aside</el-aside>
+    <el-aside class="layout-aside layout-aside-menu-200">
+      <logo></logo>
+    </el-aside>
   </div>
 </template>
 
---
+++ src/layout/layoutAside/logo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="layout-logo">
+    <img class="layout-logo-img" src="@/assets/images/favicon.png">
+    <span>vue3-Pre-admin</span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
