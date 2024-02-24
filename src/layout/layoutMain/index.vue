3e1fb519556a836ec49bf05863f16ce89cca5afe
sunjianrong <sunruolifeng@gmail.com>
2024-02-24 17:11:34 +0800

    feat: 右侧主区域指定渲染出口和过度动画缓存组件

--- src/layout/layoutMain/index.vue
+++ src/layout/layoutMain/index.vue
@@ -5,16 +5,22 @@
 <template>
   <!--  右侧主区域-->
   <el-main class="layout-main">
-    <router-view v-slot="{ Component }">
-      <transition name="fade-transform" mode="out-in">
-        <keep-alive>
-          <component :is="Component"></component>
-        </keep-alive>
-      </transition>
-    </router-view>
+    <el-scrollbar>
+      <div class="layout-main-warp">
+        <router-view v-slot="{ Component }">
+          <transition name="fade-transform" mode="out-in">
+            <keep-alive>
+              <component :is="Component"></component>
+            </keep-alive>
+          </transition>
+        </router-view>
+      </div>
+    </el-scrollbar>
   </el-main>
 </template>
 
 <style scoped>
-
+:deep(.el-scrollbar_view) {
+  height: 100%;
+}
 </style>
