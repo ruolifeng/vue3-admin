69e81368982ad56a6b0cdf7ddba66cedf8d61fe2
sunjianrong <sunruolifeng@gmail.com>
2024-02-27 14:59:44 +0800

    feat: 完善列和点击行展开收起菜单

--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts" name="SystemMenu">
 import {getList} from "@/api/system/menu";
-import {reactive, toRefs, onMounted} from "vue";
+import {reactive, toRefs, onMounted, ref} from "vue";
 
+const tableListRef = ref();
 const state = reactive({
   loading: false,
   query: {
@@ -24,18 +25,55 @@ async function queryData() {
     state.loading = false
   }
 }
+
+// 点击行，展开，收起子菜单
+function toggleRow(row: SysMenuType) {
+  console.log('toggleRow', row)
+  tableListRef.value.toggleRowExpansion(row)
+}
+
+function handleAdd(id?: string) {
+  console.log(id)
+}
+
+function handleEdit(rwo: SysMenuType) {
+
+}
+
+function handleDelete(id?: string) {
+
+}
 </script>
 
 <template>
-  <div>
+  <div class="layout-padding">
+    <el-form :inline="true" :model="query" class="demo-form-inline">
+      <el-form-item label="菜单名称">
+        <el-input v-model="query.keyWord" placeholder="请输入菜单名称" clearable/>
+      </el-form-item>
+      <el-form-item label="Activity time">
+        <el-date-picker
+            v-model="query.date"
+            type="date"
+            placeholder="Pick a date"
+            clearable
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button icon="ele-Search" @click="queryData()" type="primary">查询</el-button>
+        <el-button icon="ele-Plus" @click="handleAdd()" type="success">新增</el-button>
+      </el-form-item>
+    </el-form>
     <!--    data接收的数组元素中要有children属性，必须指定唯一标识row-key="id"-->
     <el-table
+        class="w100"
         ref="tableListRef"
         v-loading="loading"
         :data="tableList"
         row-key="id"
         border
         stripe
+        @row-click="toggleRow"
     >
       <el-table-column prop="meta.title" label="菜单名称" header-align="center" align="left" show-overflow-tooltip>
         <!--        默认插槽，上面的内容将会失效，以默认插槽中的为主，数组中的每一个对象都会传递给我我们叫做row-->
@@ -53,8 +91,15 @@ async function queryData() {
         </template>
       </el-table-column>
       <el-table-column prop="sort" label="排序" header-align="center" align="center"/>
-      <el-table-column label="操作" header-align="center" align="center">
-
+      <el-table-column label="操作" header-align="center" align="center" width="240px">
+        <template #default="{row}">
+          <el-button @click.stop="handleAdd(row.id)" v-if="row.type != 2" icon="ele-Plus" type="primary" link>新增下级
+          </el-button>
+          <el-button @click.stop="handleEdit(row.id)" v-if="row.type != 2" icon="ele-Edit" type="warning" link>修改
+          </el-button>
+          <el-button @click.stop="handleDelete(row.id)" v-if="row.type != 2" icon="ele-Delete" type="danger" link>删除
+          </el-button>
+        </template>
       </el-table-column>
     </el-table>
   </div>
