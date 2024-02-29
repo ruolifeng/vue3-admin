5bd329a66c04fb19cf944d4877487fc651bef6d5
sunjianrong <sunruolifeng@gmail.com>
2024-02-29 12:45:23 +0800

    feat: 分配权限菜单树

--- src/views/system/role/components/permission.vue
+++ src/views/system/role/components/permission.vue
@@ -42,14 +42,35 @@ function close(){
 </script>
 
 <template>
-  <el-drawer v-model="visible" :title="`分配【${role.roleName}】权限`" :direction="'rtl'" :before-close="close" size="650px">
-    <el-tree
-        :data="menuList"
-        show-checkbox
-    />
+  <el-drawer v-model="visible" :title="`分配【${role.roleName}】权限`" :direction="'rtl'" :before-close="close" size="400px">
+    <el-tree ref="treeRef" :data="menuList" show-checkbox node-key="id" :props="{children:'children'}" highlight-current>
+      <template #default="{node,data}">
+        <span class="custom-tree-node">
+<!--          一二级菜单显示原生菜单图标-->
+          <SvgIcon v-if="[1,2].includes(node.level)" :name="data.meta.icon"></SvgIcon>
+<!--          三级菜单显示菜单或者按钮图标-->
+          <el-icon v-else>
+            <ele-Menu v-if="data.type == 1"></ele-Menu>
+            <ele-SwitchButton v-if="data.type == 2"></ele-SwitchButton>
+          </el-icon>
+          <span class="ml5">{{data.meta.title}}</span>
+        </span>
+      </template>
+    </el-tree>
+    <template #footer>
+      <el-button type="success" @click="close">提交</el-button>
+      <el-button type="primary" @click="close">全选/不选</el-button>
+      <el-button @click="close">展开/收起</el-button>
+      <el-button type="danger" @click="close">取消</el-button>
+    </template>
   </el-drawer>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.custom-tree-node{
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+}
 </style>
