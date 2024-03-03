28ef7f922088c304484e8ad90716bb875289df52
sunjianrong <sunruolifeng@gmail.com>
2024-03-03 16:14:02 +0800

    feat: 用户权限路由动态加载数组并添加动态路径addRoute

--- src/router/backEnd.ts
+++ src/router/backEnd.ts
@@ -1,7 +1,15 @@
 import {useAuthStore} from "@/stores/auth";
 import {getMenuUserInfo} from "@/api/system/menu";
-import type {RouteComponent} from "vue-router";
+import type {RouteComponent, RouteRecordRaw} from "vue-router";
+import router, {defaultRoutes, dynamicRoutes} from "@/router/index";
 
+// 将文件中的组件全部一起导入
+const modules:Record<string, RouteComponent> =
+    import.meta.glob(['@/views/**/*.vue', '!@/views/**/components/**.vue']); // 匹配views目录下面的所有.vue文件，同时排除所有的子组件
+// 将匹配出来的组件前缀去除，和后端查询出来的数据进行匹配
+const viewsModules:Record<string, RouteComponent> =
+    Object.keys(modules).reduce((prevObj, currKey) =>
+        Object.assign(prevObj, {[currKey.replace(/\/src\/views|..\/views/, '')]: modules[currKey]}), {});
 export async function initBackEndRouters() {
     const authStore = useAuthStore();
     if (!authStore) return false;
@@ -11,14 +19,34 @@ export async function initBackEndRouters() {
     if (!userInfo || !menuList || menuList.length <= 0) return false;
     authStore.setUserInfo(userInfo);
     authStore.setButtonList(buttonList);
-    authStore.setMenuList(menuList);
+    // authStore.setMenuList(menuList);
+    // console.log(menuList)
+    // 将目标路由替换
+    dynamicRoutes[0].children = dynamicImportComponent(menuList);
+    authStore.setMenuList(dynamicRoutes[0].children)
+    // console.log(dynamicRoutes[0].children)
+    addRouteHandle()
+}
+export function dynamicImportComponent(dynamicRoutes: RouteRecordRaw[]): RouteRecordRaw[] {
+    if (!dynamicRoutes || dynamicRoutes.length <= 0) return [];
+    return dynamicRoutes.map((route) => {
+// 存在 component 值，则查找对应动态导入组件方法
+        const { component } = route;
+        if (component) route.component = viewsModules[`${component}`] || viewsModules[`/${component}`];
+        route.children && dynamicImportComponent(route.children);
+        return route;
+    });
+}
 
-    // 将文件中的组件全部一起导入
-    const modules:Record<string, RouteComponent> =
-        import.meta.glob(['@/views/**/*.vue', '!@/views/**/components/**.vue']); // 匹配views目录下面的所有.vue文件，同时排除所有的子组件
-    // 将匹配出来的组件前缀去除，和后端查询出来的数据进行匹配
-    const viewsModules:Record<string, RouteComponent> =
-        Object.keys(modules).reduce((prevObj, currKey) =>
-            Object.assign(prevObj, {[currKey.replace(/\/src\/views|..\/views/, '')]: modules[currKey]}), {});
-
+export function addRouteHandle() {
+    // 将静态配置的默认路由和子路由进行合并，防止之前的合并失效出现全屏404的现象
+    const layoutRouteChildren = [...dynamicRoutes[0].children || [], ...defaultRoutes]
+    // 将合并之后的路由配置到子路由中
+    dynamicRoutes[0].children = layoutRouteChildren;
+    // console.log(dynamicRoutes)
+    // 动态加载路由，将路由注册
+    dynamicRoutes.forEach((route)=>{
+        // console.log(dynamicRoutes)
+        router.addRoute(route);
+    })
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -22,119 +22,122 @@ export const dynamicRoutes: RouteRecordRaw[] = [
         component: () => import('@/layout/index.vue'),
         redirect: '/home',
         children: [
-            {
-                path: '/home',
-                name: 'Home',
-                component: () => import('@/views/home/index.vue'),
-                // 在 Vue Router 中，meta 属性是一个非常有用的特性，它可以让你为路由对象添加一些额外的自定义数据。这些数据可以在路由守卫或者组件内部访问，以便进行一些自定义的操作或者决策
-                meta: {
-                    title: '首页',
-                    icon: 'ele-HomeFilled',
-                    cache: false,
-                    hidden: false
-                }
-            },
-            {
-                path: '/system',
-                name: 'System',
-                redirect: '/system/menu',
-                meta: {
-                    title: '系统管理',
-                    icon: 'ele-Setting'
-                },
-                children: [
-                    {
-                        path: 'menu',
-                        name: 'SystemMenu',
-                        component: () => import('@/views/system/menu/index.vue'),
-                        meta: {
-                            title: '菜单管理',
-                            icon: 'ele-Menu',
-                            cache: true,
-                            hidden: false
-                        }
-                    },
-                    {
-                        path: 'role',
-                        name: 'SystemRole',
-                        component: () => import('@/views/system/role/index.vue'),
-                        meta: {
-                            title: '角色管理',
-                            icon: 'ele-Bowl',
-                            cache: true,
-                            hidden: false
-                        }
-                    },
-                    {
-                        path: 'user',
-                        name: 'SystemUser',
-                        component: () => import('@/views/system/user/index.vue'),
-                        meta: {
-                            title: '用户管理',
-                            icon: 'ele-Dish',
-                            cache: true,
-                            hidden: false
-                        }
-                    }
-                ]
-            },
-            {
-                path: '/goods',
-                name: 'Goods',
-                redirect: '/goods/list',
-                meta: {
-                    title: '商品',
-                    icon: 'ele-Goods'
-                },
-                children: [
-                    {
-                        path: 'list',
-                        name: 'SystemList',
-                        component: () => import('@/views/goods/list.vue'),
-                        meta: {
-                            title: '商品列表',
-                            icon: 'ele-Operation',
-                            cache: true,
-                            hidden: false
-                        }
-                    },
-                    {
-                        path: 'category',
-                        name: 'SystemCategory',
-                        component: () => import('@/views/goods/category/index.vue'),
-                        meta: {
-                            title: '商品分类',
-                            icon: 'ele-GoodsFilled',
-                            cache: true,
-                            hidden: false
-                        }
-                    }
-                ]
-            },
-            {
-                path: '/author',
-                name: 'Author',
-                component: () => import('@/views/author/index.vue'),
-                meta: {
-                    title: '作者主页',
-                    icon: 'ele-Link',
-                    linkTo: 'https://www.rlfit.cn'
-                }
-            },
-            // 注意，这个通配符路由应该放在所有路由配置的最后，因为 Vue Router 会按照路由配置的顺序来匹配路由，一旦找到匹配的路由，就会停止搜索。所以，如果你把通配符路由放在其他路由之前，那么所有的路径都会被它匹配，而其他的路由就永远不会被匹配到。
-            {
-                path: '/:pathMatch(.*)*',
-                name: 'NotFind',
-                component: () => import('@/views/404/index.vue'),
-                meta: {
-                    title: '404',
-                    icon: 'ele-Warning',
-                    hidden: true,
-                    cache: true
-                }
-            },
+            // {
+            //     path: '/home',
+            //     name: 'Home',
+            //     component: () => import('@/views/home/index.vue'),
+            //     // 在 Vue Router 中，meta 属性是一个非常有用的特性，它可以让你为路由对象添加一些额外的自定义数据。这些数据可以在路由守卫或者组件内部访问，以便进行一些自定义的操作或者决策
+            //     meta: {
+            //         title: '首页',
+            //         icon: 'ele-HomeFilled',
+            //         cache: false,
+            //         hidden: false
+            //     }
+            // },
+            // {
+            //     path: '/system',
+            //     name: 'System',
+            //     redirect: '/system/menu',
+            //     meta: {
+            //         title: '系统管理',
+            //         icon: 'ele-Setting'
+            //     },
+            //     children: [
+            //         {
+            //             path: 'menu',
+            //             name: 'SystemMenu',
+            //             component: () => import('@/views/system/menu/index.vue'),
+            //             meta: {
+            //                 title: '菜单管理',
+            //                 icon: 'ele-Menu',
+            //                 cache: true,
+            //                 hidden: false
+            //             }
+            //         },
+            //         {
+            //             path: 'role',
+            //             name: 'SystemRole',
+            //             component: () => import('@/views/system/role/index.vue'),
+            //             meta: {
+            //                 title: '角色管理',
+            //                 icon: 'ele-Bowl',
+            //                 cache: true,
+            //                 hidden: false
+            //             }
+            //         },
+            //         {
+            //             path: 'user',
+            //             name: 'SystemUser',
+            //             component: () => import('@/views/system/user/index.vue'),
+            //             meta: {
+            //                 title: '用户管理',
+            //                 icon: 'ele-Dish',
+            //                 cache: true,
+            //                 hidden: false
+            //             }
+            //         }
+            //     ]
+            // },
+            // {
+            //     path: '/goods',
+            //     name: 'Goods',
+            //     redirect: '/goods/list',
+            //     meta: {
+            //         title: '商品',
+            //         icon: 'ele-Goods'
+            //     },
+            //     children: [
+            //         {
+            //             path: 'list',
+            //             name: 'SystemList',
+            //             component: () => import('@/views/goods/list.vue'),
+            //             meta: {
+            //                 title: '商品列表',
+            //                 icon: 'ele-Operation',
+            //                 cache: true,
+            //                 hidden: false
+            //             }
+            //         },
+            //         {
+            //             path: 'category',
+            //             name: 'SystemCategory',
+            //             component: () => import('@/views/goods/category/index.vue'),
+            //             meta: {
+            //                 title: '商品分类',
+            //                 icon: 'ele-GoodsFilled',
+            //                 cache: true,
+            //                 hidden: false
+            //             }
+            //         }
+            //     ]
+            // },
+            // {
+            //     path: '/author',
+            //     name: 'Author',
+            //     component: () => import('@/views/author/index.vue'),
+            //     meta: {
+            //         title: '作者主页',
+            //         icon: 'ele-Link',
+            //         linkTo: 'https://www.rlfit.cn'
+            //     }
+            // },
         ]
     }
+]
+
+export const defaultRoutes: RouteRecordRaw[] = [
+    // 注意，这个通配符路由应该放在所有路由配置的最后，因为 Vue Router 会按照路由配置的顺序来匹配路由，一旦找到匹配的路由，就会停止搜索。所以，如果你把通配符路由放在其他路由之前，那么所有的路径都会被它匹配，而其他的路由就永远不会被匹配到。
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFind',
+        component: () => import('@/views/404/index.vue'),
+        meta: {
+            title: '404',
+            icon: 'ele-Warning',
+            hidden: true,
+            cache: true
+        }
+    },
 ]
 
 //全屏显示路由布局组件，不作用到布局主区域
@@ -153,7 +156,7 @@ const fullscreenRouts: RouteRecordRaw[] = [
 const router = createRouter({
     // 获取的是vite.config.ts中的环境变量的值
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [...dynamicRoutes,...fullscreenRouts]
+    routes: [...defaultRoutes,...fullscreenRouts]
 })
 
 export default router
